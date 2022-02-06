96b1438d78ae03fc2e4bad1e8b65217fe632d5e1
DanielFrauAlfaro <98766327+DanielFrauAlfaro@users.noreply.github.com>
2022-02-06 12:19:57 +0100

    Update act1.py

--- act1.py
+++ act1.py
@@ -1,17 +1,17 @@
-import cv2 as cv
+import cv2
 import numpy as np
 
 # sudo apt get install python3: para instalar el python3 por si acaso
 # pip3 install opencv-python
 
-img = cv.imread("balls.jpg")
-img = cv.resize(img, (500,500))
+img = cv2.imread("balls.jpg")
+img = cv2.resize(img, (500,500))
 
-img = cv.GaussianBlur(img,(31,31),0)
+img = cv2.GaussianBlur(img,(31,31),0)
 
 img2 = 255 - img[:,:,2]
 
-aux,dst = cv.threshold(img2,170,255,cv.THRESH_BINARY)
+aux,dst = cv2.threshold(img2,170,255,cv2.THRESH_BINARY)
 
-cv.imshow("Act1", dst)
-cv.waitKey(0)
+cv2.imshow("Act1", dst)
+#cv.waitKey(0)
